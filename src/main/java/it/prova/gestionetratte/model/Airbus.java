a0397a7e6b3833836c45a69f4e70b53ff1aff3fe
MarioOlla <mario.olla25@gmail.com>
2022-11-25 15:47:19 +0100

    implementate le classi di model per Airbus e Tratta, definito l'enum Stato

---
+++ src/main/java/it/prova/gestionetratte/model/Airbus.java
@@ -0,0 +1,102 @@
+package it.prova.gestionetratte.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "airbus")
+public class Airbus {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "codice")
+	private String codice;
+	@Column(name = "descrizione")
+	private String descrizione;
+	@Column(name = "dataInizioServizio")
+	private LocalDate dataInizioServizio;
+	@Column(name = "numeroPasseggeri")
+	private Integer numeroPasseggeri;
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "airbus")
+	private List<Tratta> tratte = new ArrayList<>();
+	
+	public Airbus() {
+		super();
+	}
+	
+	public Airbus(Long id, String codice, String descrizione, LocalDate dataInizioServizio, Integer numeroPasseggeri,
+			List<Tratta> tratte) {
+		super();
+		this.id = id;
+		this.codice = codice;
+		this.descrizione = descrizione;
+		this.dataInizioServizio = dataInizioServizio;
+		this.numeroPasseggeri = numeroPasseggeri;
+		this.tratte = tratte;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public LocalDate getDataInizioServizio() {
+		return dataInizioServizio;
+	}
+
+	public void setDataInizioServizio(LocalDate dataInizioServizio) {
+		this.dataInizioServizio = dataInizioServizio;
+	}
+
+	public Integer getNumeroPasseggeri() {
+		return numeroPasseggeri;
+	}
+
+	public void setNumeroPasseggeri(Integer numeroPasseggeri) {
+		this.numeroPasseggeri = numeroPasseggeri;
+	}
+
+	public List<Tratta> getTratte() {
+		return tratte;
+	}
+
+	public void setTratte(List<Tratta> tratte) {
+		this.tratte = tratte;
+	}
+	
+	
+	
+}
